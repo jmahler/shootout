238e5349e54c231203745e203043520896bfe270
Jeremiah Mahler <jmmahler@gmail.com>
2014-01-28 23:08:47 -0800

    fixed initial Fibonacci numbers
    The initial Fibonacci numbers were not being
    calculated properly.  Fixed so that the sequence
    starts with 0, 1, 1, 2 ...

--- fibonnacci/lisp/fibonacci.lisp
+++ fibonnacci/lisp/fibonacci.lisp
@@ -1,8 +1,11 @@
 #!/usr/bin/sbcl --script
 
 (defun fib (n)
-  (if (<= n 1)
-	1
+  "Fibonacci number, sum of previous two numbers in sequence"
+  (if (<= n 2)
+	(if (= n 0)
+	  0
+	  1)
 	(+ (fib (- n 1))
 	   (fib (- n 2)))))
 		
