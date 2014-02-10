aad3b9d9ddf20053cc7156ab4842fdc4797a3e0c
Jeremiah Mahler <jmmahler@gmail.com>
2014-02-09 22:34:33 -0800

    added Fibonacci example in Python

---
+++ fibonnacci/python/fibonacci.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+
+if 2 != len(sys.argv):
+    sys.stderr.write("usage: " + sys.argv[0] + " <n>\n")
+    sys.exit(1)
+
+n = int(sys.argv[1])
+
+def fibonacci(n):
+    if (0 == n):
+        return 0
+    elif (n >= 1 and n <= 2):
+        return 1
+
+    n = n - 2
+
+    s = 0  # sum
+    n1 = 1
+    n2 = 1
+
+    while (0 != n):
+        s = n1 + n2
+        n1 = n2
+        n2 = s
+
+        n = n - 1
+
+    return s
+
+n = fibonacci(n)
+
+print(n)
