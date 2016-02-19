4def0492dfa0bcff957d5855d0a1519ff02b220a
Jeremiah Mahler <jmmahler@gmail.com>
2016-02-19 08:30:57 -0800

    store char counts in a struct
    Store char counts in a struct so that both the char and the
    count can be stored.  This will make it possible to sort
    the list later.

--- count-char/c/count-char.c
+++ count-char/c/count-char.c
@@ -9,11 +9,24 @@
 #define OTHER_CHAR NUM_VALID_CHARS
 #define NUM_CHARS NUM_VALID_CHARS + 1  /* valid chars +1 other/invalid chars */
 
+struct char_count {
+	char c;
+	int n;
+};
+
+void init_chars(struct char_count *cs) {
+	for (int c = VALID_CHAR_START; c <= VALID_CHAR_END; c++) {
+		cs[c - VALID_CHAR_START].c = (char) c;
+	}
+}
+
 int main(int argc, char *argv[]) {
 
 	char c;
-	int cs[NUM_CHARS] = { 0 };
+	struct char_count cs[NUM_CHARS] = { 0 };
 	/* +1 for other characters */
+
+	init_chars(cs);
 
 	/* count all the characters */
 	while (read(STDIN_FILENO, &c, sizeof(c))) {
@@ -23,15 +36,16 @@ int main(int argc, char *argv[]) {
 			i = ((int) c) - VALID_CHAR_START;
 		else
 			i = OTHER_CHAR;  /* other */
-		cs[i]++;
+		cs[i].n++;
 	}
 
 	/* display the results */
 	for (int i = 0; i < NUM_VALID_CHARS; i++) {
-		char c = i + VALID_CHAR_START;
-		printf("%c %d\n", c, cs[i]);
+		char c = cs[i].c;
+		int n = cs[i].n;
+		printf("%c %d\n", c, n);
 	}
-	printf("(other) %d\n", cs[OTHER_CHAR]);
+	printf("(other) %d\n", cs[OTHER_CHAR].n);
 
 	return 0;
 }
