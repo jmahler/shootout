a221ca9834ac284665c8f29058e82d01d7ab1dc0
Jeremiah Mahler <jmmahler@gmail.com>
2017-02-04 23:53:19 -0800

    add -h help usage

--- getopt/go/getopt_go.go
+++ getopt/go/getopt_go.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/pborman/getopt/v2"
+	"os"
 )
 
 func main() {
@@ -10,11 +11,18 @@ func main() {
 	var (
 		flag bool
 		key string
+		help bool
 	)
 
+	getopt.Flag(&help, 'h', "help")
 	getopt.Flag(&flag, 'f', "flag")
 	getopt.Flag(&key, 'k', "key")
 	getopt.Parse()
+
+	if help {
+		getopt.PrintUsage(os.Stdout)
+		os.Exit(1)
+	}
 
 	fmt.Printf("flag = %t\n", flag)
 	fmt.Printf(" key = '%v'\n", key)
