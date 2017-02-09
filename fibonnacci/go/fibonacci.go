943bec75998055d107e5c673f5cb42b46c76441c
Jeremiah Mahler <jmmahler@gmail.com>
2017-02-09 08:16:49 -0800

    calculate Fibonacci number using an argument
    Calculate the Fibonacci number using an argument
    supplied by the user instead of a hard coded value.

--- fibonnacci/go/fibonacci.go
+++ fibonnacci/go/fibonacci.go
@@ -7,11 +7,15 @@
 
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+	"strconv"
+)
 
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
+// A function that each time it is called
+// returns the next Fibonacci number.
+func fibonacci_step() func() int {
 	a := 0
 	b := 1
 
@@ -25,9 +29,22 @@ func fibonacci() func() int {
 	}
 }
 
-func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+// Calculate the nth Fibonacci number.
+func fibonacci(n int) int {
+	fn := fibonacci_step()
+
+	for i := 0; i < n; i++ {
+		fn()
 	}
+
+	return fn()
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("%v <n>\n", os.Args[0])
+		os.Exit(2)
+	}
+	n, _ := strconv.Atoi(os.Args[1])
+	fmt.Println(fibonacci(n))
 }
