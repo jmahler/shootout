d97c17af811a3648b610336be5900a68c81a575a
Jeremiah Mahler <jmmahler@gmail.com>
2018-06-14 01:28:15 +0000

    simplify using fileinput

--- tac-revlines/python3/tac.py
+++ tac-revlines/python3/tac.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
+import fileinput
 
 if __name__ == '__main__':
     lines = []
 
-    infd = sys.stdin
-    if len(sys.argv) > 1:
-        infd = open(sys.argv[1])
-
-    for line in infd:
-        lines.append(line)
+    with fileinput.input() as infd:
+        for line in infd:
+            lines.append(line)
 
     # print lines in reverse order
     for line in lines[::-1]:
