fa77ef14710f4c7b47418cd7e34a69b48af9ed73
Jeremiah Mahler <jmmahler@gmail.com>
2023-03-02 08:34:32 -0800

    support cat of multiple files

--- cat/go/cat.go
+++ cat/go/cat.go
@@ -10,20 +10,22 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "usage: cat <file>")
+		fmt.Fprintln(os.Stderr, "usage: cat <file> [<file> ...]")
 		os.Exit(1)
 	}
-	file := os.Args[1]
+	for i := 1; i < len(os.Args); i++ {
+		file := os.Args[i]
 
-	fd, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
+		fd, err := os.Open(file)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	rd := bufio.NewReader(fd)
+		rd := bufio.NewReader(fd)
 
-	_, err = io.Copy(os.Stdout, rd)
-	if err != nil {
-		log.Fatal(err)
+		_, err = io.Copy(os.Stdout, rd)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
